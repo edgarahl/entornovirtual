a4019f163059e3799352e4d2acecfa3e7c540f21
edgarahl <edgarahl@gmail.com>
2020-05-08 11:58:39 -0600

    Directorio del Proyecto

--- entvirt/script/proyecto/app/models.py
+++ entvirt/script/proyecto/app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 # Create your models here.
 class Categoria(models.Model):
     descripcion = models.CharField(
@@ -15,7 +14,21 @@ class Categoria(models.Model):
 
     def save(self):
         self.descripcion = self.descripcion.upper()
-        super(Categoria, self).save()
+        super(Categoria,self).save()
 
     class Meta:
-        verbose_name_plural = "Categorias"
+        verbose_name_plural="Categorias"
+    
+class SubCategoria(models.Model):
+    categoria=models.ForeignKey(Categoria,on_delete=models.CASCADE)
+    descripcion = models.CharField(
+        max_length=50,
+        blank=False,
+        null=False
+    )
+
+    def __str__(self):
+        return "{} --> {}".format(self.categoria, self.descripcion)
+    
+    class Meta:
+        verbose_name_plural = "Sub Categorias"
