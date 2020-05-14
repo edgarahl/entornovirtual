1c9a7e4df4fbed520108447acd8b9dd2586e4450
edgarahl <edgarahl@gmail.com>
2020-05-14 12:03:36 -0600

    se agregan las urls para hospitalizaciones

--- entvirt/script/proyecto_tarea/cha_app/urls.py
+++ entvirt/script/proyecto_tarea/cha_app/urls.py
@@ -21,7 +21,7 @@ from .views import VendedorListar, VendedorNueva, VendedorBorrar, VendedorEditar
     , HospitalListar, HospitalNuevo, HospitalEditar, HospitalBorrar, AseguradoNuevo, AseguradoEditar, AseguradoBorrar, \
     AseguradoListar, PolizaEditar, PolizaBorrar, index, ContratoPoliza, ContratoPolizaListar, home, TemplateSinPrivilegio, \
     DoctorNuevo, DoctorBorrar, DoctorEditar, DoctorListar, FamiliaresBorrar, FamiliaresEditar, FamiliaresListar, \
-    FamiliaresNuevo
+    FamiliaresNuevo, HospitalizacionNuevo, HospitalizacionBorrar, HospitalizacionListar, HospitalizacionEditar
 from .reportes import reporte_vendedores
 
 urlpatterns = [
@@ -30,18 +30,22 @@ urlpatterns = [
     path("vendedores/nuevo/", VendedorNueva.as_view(), name="vendedor_nuevo"),
     path("vendedores/editar/<int:pk>", VendedorEditar.as_view(), name="vendedor_editar"),
     path("vendedores/borrar/<int:pk>", VendedorBorrar.as_view(), name="vendedor_borrar"),
+
     path("polizas/", PolizaListar.as_view(), name="poliza_listar"),
     path("polizas/nuevo/", PolizaNueva.as_view(), name="poliza_nuevo"),
     path("polizas/editar/<int:pk>", PolizaEditar.as_view(), name="poliza_editar"),
     path("polizas/borrar/<int:pk>", PolizaBorrar.as_view(), name="poliza_borrar"),
+
     path("hospitales/", HospitalListar.as_view(), name="hospital_listar"),
     path("hospitales/nuevo/", HospitalNuevo.as_view(), name="hospital_nuevo"),
     path("hospitales/editar/<int:pk>", HospitalEditar.as_view(), name="hospital_editar"),
     path("hospitales/borrar/<int:pk>", HospitalBorrar.as_view(), name="hospital_borrar"),
+
     path("asegurados/nuevo/", AseguradoNuevo.as_view(), name="asegurado_nuevo"),
     path("asegurados/editar/<int:pk>", AseguradoEditar.as_view(), name="asegurado_editar"),
     path("asegurados/borrar/<int:pk>", AseguradoBorrar.as_view(), name="asegurado_borrar"),
     path("asegurados/", AseguradoListar.as_view(), name="asegurado_listar"),
+
     # path("", views.loginpage, name="loginpage"),
     # path("login/", views.loginpage, name="loginpage"),
     path("", auth_views.LoginView.as_view(template_name='cha_app/login.html'), name="login"),
@@ -50,13 +54,21 @@ urlpatterns = [
     # path("login/", auth_views.LoginView.as_view(template_name='cha_app/login.html'), name="login"),
     path("contratos/", ContratoPolizaListar.as_view(), name="contrato_listar"),
     path("contratos/nuevo/", ContratoPoliza.as_view(), name="contrato_nuevo"),
+
     path("doctor/nuevo/", DoctorNuevo.as_view(), name="doctor_nuevo"),
     path("doctor/editar/<int:pk>", DoctorEditar.as_view(), name="doctor_editar"),
     path("doctor/borrar/<int:pk>", DoctorBorrar.as_view(), name="doctor_borrar"),
     path("doctor/", DoctorListar.as_view(), name="doctor_listar"),
+
     path("familiares/nuevo/", FamiliaresNuevo.as_view(), name="familiares_nuevo"),
     path("familiares/editar/<int:pk>", FamiliaresEditar.as_view(), name="familiares_editar"),
     path("familiares/borrar/<int:pk>", FamiliaresBorrar.as_view(), name="familiares_borrar"),
     path("familiares/", FamiliaresListar.as_view(), name="familiares_listar"),
+
     path("reportes/vendedores/", reporte_vendedores, name='vendedores_print_all'),
+
+    path("hospitalizaciones/nuevo/", HospitalizacionNuevo.as_view(), name="hospitalizaciones_nuevo"),
+    path("hospitalizaciones/editar/<int:pk>", HospitalizacionEditar.as_view(), name="hospitalizaciones_editar"),
+    path("hospitalizaciones/borrar/<int:pk>", HospitalizacionBorrar.as_view(), name="hospitalizaciones_borrar"),
+    path("hospitalizaciones/", HospitalizacionListar.as_view(), name="hospitalizaciones_listar"),
 ]
